461b174b4131f57396954aa1b49823c1833f735f
cti1650 <cti1650@gmail.com>
2022-04-17 02:06:35 +0900

    ✨ feat: Recoil導入

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
-import '@styles/globals.css';
+import '@styles/globals.css'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className='bg-[#F8F8F8] p-[25px]'>
-      <Component {...pageProps} />
-    </div>
-  );
+    return (
+        <div className='bg-[#F8F8F8] p-[25px]'>
+            <RecoilRoot>
+                <Component {...pageProps} />
+            </RecoilRoot>
+        </div>
+    )
 }
 
-export default MyApp;
+export default MyApp
