d6ee3e42b5676ac954f2fb6a56e46bc6f36d9b28
yugo-ibuki <y.ibuki91@gmail.com>
2022-04-17 23:52:35 +0900

    refactor(icon): import / exportのリファクタリング

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,25 +1,11 @@
-import { CoffeeIcon } from './CoffeeIcon'
-import { FocusIcon } from './FocusIcon'
-import { LockIcon } from './LockIcon'
-import { UnLockIcon } from './UnLockIcon'
-import { OffMicIcon } from './OffMicIcon'
-import { OnMicIcon } from './OnMicIcon'
-import { OffVolumeIcon } from './OffVolumeIcon'
-import { OnVolumeIcon } from './OnVolumeIcon'
-import { ScreenIcon } from './ScreenIcon'
-import { LogoutIcon } from './LogoutIcon'
-import { ClosedIcon } from './ClosedIcon'
-
-export {
-    CoffeeIcon,
-    FocusIcon,
-    LockIcon,
-    UnLockIcon,
-    OffMicIcon,
-    OnMicIcon,
-    OffVolumeIcon,
-    OnVolumeIcon,
-    ScreenIcon,
-    LogoutIcon,
-    ClosedIcon,
-}
+export * from './CoffeeIcon'
+export * from './FocusIcon'
+export * from './LockIcon'
+export * from './UnLockIcon'
+export * from './OffMicIcon'
+export * from './OnMicIcon'
+export * from './OffVolumeIcon'
+export * from './OnVolumeIcon'
+export * from './ScreenIcon'
+export * from './LogoutIcon'
+export * from './ClosedIcon'
